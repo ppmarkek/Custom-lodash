d49fad6904ea1de79b9c048c1865c9e1d9fbf841
ppmarkek <markungeitis01@gmail.com>
2023-02-18 20:19:58 +0200

    feat: fix filter and dropWhile

--- lodash.js
+++ lodash.js
@@ -20,15 +20,25 @@ class ImdenverLodash {
       return newMap;
      } else if(typeof fun === 'object' && !Array.isArray(fun)){
       const newMap = [];
-      arr.map((x) => x.user !== fun.user || x.active !== fun.active ? newMap.push(x.user) : 0);
+      let key1;
+      let key2;
+      const funKeys = Object.keys(fun);
+      const funValue = Object.values(fun);
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === funKeys[0] ? key1 = lenght : 0));
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === funKeys[1] ? key2 = lenght : 0));
+      arr.map((x) => Object.keys(x)[key1] === funKeys[0] && Object.values(x)[key1] === funValue[0] && Object.keys(x)[key2] === funKeys[1] && Object.values(x)[key2] === funValue[1] ? 0 : newMap.push(x.user));
       return newMap;
      } else if(typeof fun === 'object' && Array.isArray(fun)){
       const newMap = [];
-      arr.some((x) => fun[0] === 'active' && x.active !== fun[1] ? newMap.push(x.user) : 0);
+      let key;
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === fun[0] ? key = lenght : 0));
+      arr.some((x) => Object.keys(x)[key] === fun[0] && Object.values(x)[key] !== fun[1] ? newMap.push(x.user) : 0);
       return newMap;
      } else if(typeof fun === 'string'){
       const newMap = [];
-      arr.map((x) => fun === 'active' ? newMap.push(x.user) : newMap.push(x.active));
+      let key;
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === fun ? key = lenght : 0));
+      arr.some((x) => Object.keys(x)[key] === fun ? newMap.push(x.user) : 0);
       return newMap;
      }
   }
@@ -43,15 +53,25 @@ class ImdenverLodash {
       return newMap;
      } else if(typeof fun === 'object' && !Array.isArray(fun)){
       const newMap = [];
-      arr.map((x) => x.user === fun.user || x.active === fun.active ? newMap.push(x.user) : 0);
+      let key1;
+      let key2;
+      const funKeys = Object.keys(fun);
+      const funValue = Object.values(fun);
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === funKeys[0] ? key1 = lenght : 0));
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === funKeys[1] ? key2 = lenght : 0));
+      arr.some((x) => Object.keys(x)[key1] === funKeys[0] && Object.values(x)[key1] === funValue[0] && Object.keys(x)[key2] === funKeys[1] && Object.values(x)[key2] === funValue[1] ? newMap.push(x.user) : 0);
       return newMap;
      } else if(typeof fun === 'object' && Array.isArray(fun)){
       const newMap = [];
-      arr.some((x) => fun[0] === 'active' && x.active === fun[1] ? newMap.push(x.user) : 0);
+      let key;
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === fun[0] ? key = lenght : 0));
+      arr.some((x) => Object.keys(x)[key] === fun[0] && Object.values(x)[key] === fun[1] ? newMap.push(x.user) : 0);
       return newMap;
      } else if(typeof fun === 'string'){
       const newMap = [];
-      arr.some((x) => fun === 'active' ? newMap.push(x.user) : newMap.push(x.active));
+      let key;
+      arr.some((x) => Object.keys(x).map((y, lenght) => y === fun ? key = lenght : 0));
+      arr.some((x) => Object.keys(x)[key] === fun ? newMap.push(x.user) : 0);
       return newMap;
      }
   }
@@ -88,12 +108,11 @@ class ImdenverLodash {
 
 const lod = new ImdenverLodash;
 var users = [
-  { 'user': 'barney',  'age': 36, 'active': true },
-  { 'user': 'fred',    'age': 40, 'active': false },
-  { 'user': 'pebbles', 'age': 1,  'active': true }
+  { 'user': 'barney', 'age': 36, 'active': true },
+  { 'user': 'fred',   'age': 40, 'active': false }
 ];
    
-lod.find(users, { 'age': 1, 'active': true });
+lod.filter(users, 'active');
 
 
 
